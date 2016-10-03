0d9191cdf7314331fda095f448f5c3f2872bbaa8
Karolina Blumke <k.blumke@clearcode.cc>
2016-10-03 16:17:01 +0200

    Step nr 2 - Flask-Tracking

--- flask_tracking/__init__.py
+++ flask_tracking/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 
-from .models import db
-from .views import tracking
+from .auth import login_manager
+from .data import db
+from .tracking.views import tracking
+from .users.views import users
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+
 db.init_app(app)
 
+login_manager.init_app(app)
+
 app.register_blueprint(tracking)
+app.register_blueprint(users)
